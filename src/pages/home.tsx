faf0362e431f5810dbe63438408a032cb80f488c
subharakesh <subhalakshmip@gmail.com>
2025-12-27 17:50:43 -0600

    home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,49 +1,39 @@
-import { Suspense } from 'react';
-import Footer from '@/components/footer';
-import Hero from '../components/hero'
+import { Suspense, lazy } from "react";
+import Footer from "@/components/footer";
 
-import HeroCar from '@/components/heroCar';
-
+const Hero = lazy(() => import("@/components/hero"));
+const HeroCar = lazy(() => import("@/components/heroCar"));
+const RecentlyViewed = lazy(() => import("@/components/recentlyViewed"));
 import { InfoSection } from "@/components/infoSection";
-
-import RecentlyViewed from '@/components/recentlyViewed';
-
-
-
-
-
 
 const HomePage = () => {
   return (
-    <div>
-     
-       <main>
-              <Suspense fallback={<div>Loading...</div>}>
-                   <Hero />
-             </Suspense>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hero />
+      </Suspense>
 
-               <h1 className="text-2xl md:text-3xl font-semibold text-[#00022E] mb-1 mt-5 ml-24  ">
-                   Featured Cars
-                </h1>
-                <h2 className="text-sm md:text-base font-normal text-gray-500 mb-10 ml-24 ">Handpicked deals just for you</h2>  
-                 
-       
-                <HeroCar />
+      <h1 className="text-2xl md:text-3xl font-semibold text-[#00022E] mb-1 mt-5 ml-24">
+        Featured Cars
+      </h1>
 
-               <h3 className="text-3xl md:text-3xl font-semibold text-[#00022E] mb-3 mt-20 ml-25 font-serif ">The Carverse difference</ h3>
-               <InfoSection />
-   
+      <h2 className="text-sm md:text-base font-normal text-gray-500 mb-10 ml-24">
+        Handpicked deals just for you
+      </h2>
 
-        
+      <HeroCar />
 
+      <h3 className="text-3xl font-semibold text-[#00022E] mb-3 mt-20 ml-24 font-serif">
+        The Carverse difference
+      </h3>
 
-               <RecentlyViewed />  
-        </main>
-     <Footer />
-      
-    </div>
-      
-  )
-}
+      <InfoSection />
 
-export default HomePage
+      <RecentlyViewed />
+
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
